398b37a83f275258c9f66691c1846d81c5608e80
jabim <97878452+JeongSabin@users.noreply.github.com>
2025-02-02 15:44:27 +0900

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,7 @@ import BlogContent from "@/components/BlogContent";
 
 export const metadata = {
   title: "Blog",
-  description: "시훈의 개발노트의 블로그 게시글 리스트입니다.",
+  description: "요모조모 블로그 게시글 리스트입니다.",
 };
 
 export default function BlogPage() {
